5a784e192859b01fb8dec9036729617169982084
sringsmuth <144136966+sringsmuth@users.noreply.github.com>
2024-04-29 09:29:16 -0500

    Removed references to Alma
    When the Library switched from Alma/Primo to Folio and EDS, the Alma special accounts were no longer needed. This Python code has been updated such that it no longer produces a file for Alma accounts as one of its outputs.

--- local_accounts.py
+++ local_accounts.py
@@ -3,7 +3,6 @@ Local Accounts
 This script creates comma-separated (.csv) files that assist in creating local/temporary accounts for EZproxy and Primo. Use this script when creating accounts for users from special programs that do not have matriculated status but need access to library resources. The script takes a comma-separated (.csv) file with three columns describing users: `first`, `last`, and `email`. It produces several text-based files:
 - `special_accounts.csv`: user accounts and passwords for giving to the program coordinator after setting up the accounts
 - `ezproxy_accounts.txt`: account information for pasting into the `LocalAccounts.txt` configuration file on the EZproxy server
-- `alma_accounts.csv`: account information for uploading accounts into Alma/Primo using the CSV uploader tool
 This script requires Python 3 to run and the `pandas` package. See the `requirements.txt` file.
 """
 
@@ -14,7 +13,7 @@ import datetime
 
 def main():
     # Read in CSV file from program coordinator
-    local = pd.read_csv('local.csv')
+    local = pd.read_csv('/Users/ringsmu/Desktop/local/local.csv')
 
     # Get user expiration date and info for comment in EZproxy file
     initials = input('Your initials: ').upper()
@@ -58,16 +57,13 @@ def main():
 
     # CSV file for program coordinator
     coordinator = local[['first', 'last', 'email', 'username', 'password']]
-    coordinator.to_csv('special_accounts.csv', index=False)
-    # CSV file for Alma
-    alma = local[['first', 'last', 'email', 'username', 'password', 'expires', "campus", "user_group", "purge", "address", "address_type", "email_type"]]
-    alma.to_csv('alma_accounts.csv', index=False)
+    coordinator.to_csv('/Users/ringsmu/Desktop/local/special_accounts.csv', index=False)
     # Text file for EZproxy
     ezproxy_accounts = local[['ezproxy']]
     new_heading = columns = '# ' + program + ' ' + datetime.datetime.today().strftime("%Y-%m-%d") + \
                                ' ' + '(' + initials + ')'
     ezproxy_accounts = ezproxy_accounts.rename(columns={'ezproxy': new_heading})
-    ezproxy_accounts.to_csv('ezproxy_accounts.txt', index=False)   
+    ezproxy_accounts.to_csv('/Users/ringsmu/Desktop/local/ezproxy_accounts.txt', index=False)   
 
 def password():
     '''
